33e280312c166984bec0edc71cd776ded5ffda82
fred <fredericond@hotmail.com>
2017-08-27 15:46:02 -0300

    mudei a variavel para testar

--- app/src/main/java/com/appdiarista/appdiarista/login.java
+++ app/src/main/java/com/appdiarista/appdiarista/login.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login extends AppCompatActivity {
-
+    private String login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
