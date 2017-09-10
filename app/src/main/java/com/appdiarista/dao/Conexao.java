b421dc7fb3da17c25b1472d7dccebf0aecd057ae
fred <fredericond@hotmail.com>
2017-09-10 16:04:48 -0300

    nova tentativa bd

--- app/src/main/java/com/appdiarista/dao/Conexao.java
+++ app/src/main/java/com/appdiarista/dao/Conexao.java
@@ -1,8 +1,7 @@
 package com.appdiarista.dao;
 
 import android.database.sqlite.SQLiteDatabase;
-
-import com.appdiarista.util.DataBaseSql;
+import android.util.Log;
 
 /**
  * Created by FR£D on 09/09/2017.
@@ -12,18 +11,18 @@ public class Conexao {
     private static SQLiteDatabase db;
 
     public static SQLiteDatabase  getBd(){
-//        File file = new File("/sdcard/bd_interno/");
-//        db = SQLiteDatabase.openOrCreateDatabase(Environment.getExternalStorageDirectory()+"/bd_interno/"+"appdiarista.db", null);
-//        if(file.listFiles() != null)
-//            for(File ff : file.listFiles())
-//                Log.i("msg","-"+ff.getName());
         if(db == null) {
-            db = SQLiteDatabase.openOrCreateDatabase("/sdcard/bd_interno/appdiarista.db", null);
-            db. execSQL(DataBaseSql.CTTIPOUSUARIO);
-            db. execSQL(DataBaseSql.CTUSUARIO);
-            db.execSQL("insert into tipoUsuario(id,descricao) values (1,'DIARISTA');");
-            db.execSQL("insert into tipoUsuario(id,descricao) values (2,'CONTRATANTE');");
-            db.execSQL("insert into usuario (login,senha,tipoUsuario_id) values('fredericond@hotmail.com','1234',2);");
+            db = SQLiteDatabase.openOrCreateDatabase("/sdcard/bd_interno/diaristaapp.db", null);
+            if(db.isOpen()){
+                Log.i("msg","open");
+            }else{
+                Log.i("msg","fechado");
+            }
+//            db. execSQL(DataBaseSql.CTTIPOUSUARIO);
+//            db. execSQL(DataBaseSql.CTUSUARIO);
+//            db.execSQL("insert into tipoUsuario(id,descricao) values (1,'DIARISTA');");
+//            db.execSQL("insert into tipoUsuario(id,descricao) values (2,'CONTRATANTE');");
+//            db.execSQL("insert into usuario (login,senha,tipoUsuario_id) values('fredericond@hotmail.com','1234',2);");
         }
         return db;
     }
